56f8d489422078128f0aa435f6ab458a91fde1a1
Eduardo Gomes <dado_v@hotmail.com>
2025-03-03 17:28:24 -0300

    gráfico de barras de quantidade por status de expiração utilizando echarts-for-react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import EChartsTest from "./components/EChartsTest";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EChartsTest />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EChartsTest.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import ReactECharts from "echarts-for-react";
+
+const EChartsTest = () => {
+  const [dados, setDados] = useState([]);
+  const [countByStatus, setCountByStatus] = useState({});
+
+  useEffect(() => {
+    fetch("/files/data.csv")
+      .then((r) => r.text())
+      .then((text) => {
+        const data = Papa.parse(text, { header: true, skipEmptyLines: true });
+        console.log(data.data);
+        setDados(data.data);
+
+        const countStatus = data.data.reduce((acc, cur) => {
+          acc[cur.status_expiracao] = (acc[cur.status_expiracao] || 0) + 1;
+          return acc;
+        }, {});
+
+        setCountByStatus(countStatus);
+        console.log(`countStatus: ${JSON.stringify(countStatus)}`);
+      });
+  }, []);
+
+  const colors = {
+    A: "#FF5733", // Vermelho
+    P: "#FFC300", // Amarelo
+    E: "#4CAF50", // Verde
+  };
+
+  const option = {
+    title: {
+      text: "Análise de Expiração",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "axis",
+    },
+    xAxis: {
+      data: Object.keys(countByStatus),
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        name: "Quantidade",
+        type: "bar",
+        data: Object.keys(countByStatus).map((key) => ({
+          value: countByStatus[key],
+          itemStyle: {
+            color: colors[key] || "#000000",
+          },
+        })),
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1>My ECharts Test</h1>
+      <ReactECharts option={option} style={{ height: 400 }} />
+      <table className="table table-striped table-bordered">
+        <caption>Lista de Dados</caption>
+        <colgroup>
+          <col style={{ width: "10%" }} />
+          <col style={{ width: "20%" }} />
+          <col style={{ width: "70%" }} />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados.map((dado, index) => (
+            <tr key={index}>
+              <td>{dado.id}</td>
+              <td>{dado.status_expiracao}</td>
+              <td>{dado.nome}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default EChartsTest;
